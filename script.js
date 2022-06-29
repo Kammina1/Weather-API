e89748665a7014e923e1d6e7d5abfca204ca1a79
Kammin Avery <kammin.avery@gmail.com>
2022-06-28 18:30:26 -0600

    added lots of JS

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 var pastCity = ["Salt Lake", "Utah", "Raleigh", "Austin", "Shit"]
+var dateEl = document.getElementById("date")
+var currentTemp;
+var currentWind;
+var currentHumidity;
+var currentUvi;
+var currentTemp = document.getElementById("current-temp")
+const API_KEY = "5a34ba45183b84a351de83286e31ee9d"
+var citySearched = document.getElementById("search-area").value;
+console.log(citySearched);
 
 function pastCitySearches() {
-    console.log(pastCity)
     var searchedCities = document.getElementById("searchedCities")
     pastCity.forEach (city => {
         var cityDiv = document.createElement("div")
@@ -18,3 +26,23 @@ function pastCitySearches() {
     
 }
 pastCitySearches();
+
+function getWeatherData() {
+    navigator.geolocation.getCurrentPosition((success) => {
+        console.log(success)
+        let {latitude, longitude} = success.coords
+        fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely,alerts,daily&units=imperial&appid=${API_KEY}`).then(res => res.json()).then(data => {
+            console.log(data)
+        showWeather(data);
+        }) 
+    })
+}
+getWeatherData();
+
+function showWeather(data) {
+    let {temp, humidity, wind_speed, uvi} = data.current;
+    var currentTemp = temp
+    var currentWind = wind_speed
+    var currentHumidity = humidity
+    var currentUvi = uvi
+}
